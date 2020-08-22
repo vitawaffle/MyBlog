cac3b08adb40edeb64b07c3a500d7d969579c95d
vit <vitaly.lobatsevich@gmail.com>
2020-08-22 16:23:07 +0300

    Base project

---
+++ src/test/java/by/vit/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vit.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
